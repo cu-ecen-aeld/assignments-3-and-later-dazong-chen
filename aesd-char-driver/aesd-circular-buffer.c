f9bd489216354e3eb81ea87e736b4830bb37edd9
dazong chen <sunnysimon1995@gmail.com>
2021-10-16 08:45:51 -0600

    part 1 full test passed

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -20,7 +20,7 @@
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
  *      character index if all buffer strings were concatenated end to end
- * @param entry_offset_byte_rtn is a pointer specifying a location to store the byte of the returned aesd_buffer_entry
+ * @param entry_offset_byte_rtn     is a pointer specifying a location to store the byte of the returned aesd_buffer_entry
  *      buffptr member corresponding to char_offset.  This value is only set when a matching char_offset is found
  *      in aesd_buffer. 
  * @return the struct aesd_buffer_entry structure representing the position described by char_offset, or
@@ -29,10 +29,46 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
 			size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+    int        cur_total_size = 0;
+    int        prev_total_size = 0;
+    uint8_t    position = buffer->out_offs;
+    
+    
+    if(buffer == NULL)
+    {
+        return NULL;
+    }
+    
+    // check if ring buffer is empty
+    if( (position == buffer->in_offs) && (buffer->full == false) )
+    {
+        return NULL;
+    }
+    
+    
+    while(position != buffer->in_offs)    // search through the circular buffer
+    {
+        if(char_offset > cur_total_size)
+        {
+            cur_total_size += buffer->entry[position].size;
+        }
+        
+        else
+        {
+            *entry_offset_byte_rtn = char_offset - prev_total_size;
+            
+            return buffer->entry[position];
+        }
+        
+        position = (position+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;  // next entry[i]
+        prev_total_size += buffer->entry[position].size;
+        
+    }
+    
+    
+    return NULL;    // searched through whole buffer, char offset is greater than total size of buffer string
+    
+    
 }
 
 /**
@@ -44,9 +80,31 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description 
-    */
+
+    if(buffer == NULL)
+    {
+        return NULL;
+    }
+       
+    if(buffer->full == true)
+    {   
+        buffer->entry[buff->in_offs] = *add_entry;    // overwrites the oldest data
+        
+        buffer->out_offs = (buffer->out_offs+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;    //advances buffer->out_offs to the new start location.
+    }
+    
+    else
+    {
+        buffer->entry[buffer->in_offs] = *add_entry;
+    }
+    
+    buffer->in_offs = (buffer->in_offs+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;    // implement in_offs to next location after new entry is written
+    
+    
+    if(buffer->in_offs == buffer->out_offs)    // check the full conditions
+    {
+        buffer->full = true;
+    }
 }
 
 /**
